0c1478c881e96cc9a7a3e3f354ee014ae8aecf6b
jdburton0 <jdburton0@CC270-86282.frostburg.edu>
2018-09-07 09:51:21 -0400

    Set up problem2 class - BurtonJLab00

---
+++ COSC310_ASN/src/edu/frostburg/cosc310/lab00/BurtonJLab00.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.frostburg.cosc310.lab00;
+
+/**
+ *
+ * @author jdburton0
+ */
+public class BurtonJLab00 implements Lab00{
+
+    @Override
+    public long problem0(int size) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long problem1(int size) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long problem2() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
