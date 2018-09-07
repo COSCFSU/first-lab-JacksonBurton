9cf3a12efc5e6ad77d292b1f4a56dad180591448
jdburton0 <jdburton0@CC277-88848.frostburg.edu>
2018-09-07 10:48:22 -0400

    Finished problem 2, added a constructor to BurtonJLab00 and Main to test

---
+++ COSC310_ASN/src/edu/frostburg/cosc310/lab00/MainTestLab00.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.frostburg.cosc310.lab00;
+
+/**
+ *
+ * @author jdburton0
+ */
+public class MainTestLab00 {
+    
+    public static void main(String[] args) {
+        BurtonJLab00 test = new BurtonJLab00(1000000);
+    }
+}
